dde167fd166905bf86f301e9ff131607c0e100e6
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-13 04:33:50 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
+
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>,
+// );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+  // { path: "/", element: <HomePage /> },
+  // { path: "/products", element: <ProductsPage /> },
+]);
+
+// const router = createBrowserRouter(routerDefinitions);
+
 function App() {
-  return <div>Router</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
